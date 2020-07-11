fd09e7736b11f89b4f1d3dd47c8304b338b9832e
sheikh-fahad-aq <fahadahmedqureshi.93@gmail.com>
2020-07-12 00:14:05 +0500

    Updated Header Footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Cards, CountryPicker, Chart } from './components';
+import { Header,Footer, Cards, CountryPicker, Chart } from './components';
 import { fetchData } from './api/';
 import styles from './App.module.css';
 
@@ -28,12 +28,17 @@ class App extends React.Component {
     const { data, country } = this.state;
 
     return (
-      <div className={styles.container}>
-        <img className={styles.image} src={image} alt="COVID-19" />
-        <Cards data={data} />
-        <CountryPicker handleCountryChange={this.handleCountryChange} />
-        <Chart data={data} country={country} /> 
+      <div>
+          <Header />
+          <div className={styles.container}>
+            <img className={styles.image} src={image} alt="COVID-19" />
+            <Cards data={data} />
+            <CountryPicker handleCountryChange={this.handleCountryChange} />
+            <Chart data={data} country={country} /> 
+           </div>
+        <Footer />
       </div>
+      
     );
   }
 }
